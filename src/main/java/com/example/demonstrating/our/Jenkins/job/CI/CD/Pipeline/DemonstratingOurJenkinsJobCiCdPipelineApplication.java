cdd16a0509a88dc558b75b3d4cb6b7cb7474f74a
Venkat Satish <venkatsatish.bv@gmail.com>
2024-07-11 18:00:55 +0530

    Update pom.xml, Dockerfile, Jenkinsfile, and other files for CI/CD pipeline

---
+++ src/main/java/com/example/demonstrating/our/Jenkins/job/CI/CD/Pipeline/DemonstratingOurJenkinsJobCiCdPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.demonstrating.our.Jenkins.job.CI.CD.Pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemonstratingOurJenkinsJobCiCdPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemonstratingOurJenkinsJobCiCdPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demonstrating/our/Jenkins/job/CI/CD/Pipeline/DemonstratingOurJenkinsJobCiCdPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demonstrating.our.Jenkins.job.CI.CD.Pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemonstratingOurJenkinsJobCiCdPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
